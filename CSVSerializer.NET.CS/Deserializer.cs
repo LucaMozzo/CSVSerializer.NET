41619c8cb6e85eb5e87189077369cadc0646c2ee
lucamozzo <lucamozzo21173@gmail.com>
2016-01-29 12:47:40 +0000

    added deserializer function

--- CSVSerializer.NET.CS/Deserializer.cs
+++ CSVSerializer.NET.CS/Deserializer.cs
@@ -25,14 +25,58 @@ namespace CSVSerializer.NET.CS
 
         public Document Deserialize()
         {
-
-            return new Document(Headers, Rows);
+            Document doc = new Document();
+            int index = 0;
+            while (!File.EndOfStream)
+            {
+                Row row = new Row();
+                char[] line = File.ReadLine().ToCharArray();
+                char[] buffer = new char[line.Length]; //temporary buffer for composing values
+                for (int i = 0; i < line.Length; i++)
+                {
+                    int bufferIndex = 0;
+                    if (line[i] == '\"')
+                    {
+                        int closingQuoteIndex = IndexOf(line, '\"', i + 1);
+                        for (int a = i + 1; a < closingQuoteIndex; a++)
+                        {
+                            buffer[bufferIndex] = line[a];
+                            ++bufferIndex;
+                        }
+                        i = closingQuoteIndex;
+                    }           
+                    else if (line[i] == ',')
+                    {
+                        String tmp = "";
+                        foreach (char c in buffer)
+                            tmp += c;
+                        Value value = new Value(tmp);
+                        row.AddValue(value);
+                    }
+                    else
+                    {
+                        buffer[bufferIndex] = line[i];
+                        ++bufferIndex;
+                    }
+                }
+                if (index == 0)
+                    doc.SetHeader(row);
+                else
+                    doc.AddRow(row);
+                ++index;
+            }
+            
         }
 
-        public List<Row> GetValues()
+        //returns the index of the first occurrence of the spacified element
+        private int IndexOf(char[] array, char element, int startingIndex = 0)
         {
-
-            return new List<Row>(); //temporary
+            for(int i = startingIndex; i < array.Length; i++)
+            {
+                if (array[i] == element)
+                    return i;
+            }
+            return -1;
         }
     }
 }
